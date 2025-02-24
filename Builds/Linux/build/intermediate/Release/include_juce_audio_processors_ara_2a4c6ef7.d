e6025f45fb95bdf620bbce209f1a21508d5dadbe
DaforLynx <daforlynx@gmail.com>
2025-02-24 01:29:33 -0800

    Fix Linux compile

---
+++ Builds/Linux/build/intermediate/Release/include_juce_audio_processors_ara_2a4c6ef7.d
@@ -0,0 +1,9 @@
+build/intermediate/Release/include_juce_audio_processors_ara_2a4c6ef7.o: \
+ ../../JuceLibraryCode/include_juce_audio_processors_ara.cpp \
+ ../../JuceLibraryCode/AppConfig.h \
+ ../../JuceLibraryCode/JucePluginDefines.h \
+ /usr/share/juce/modules/juce_audio_processors/juce_audio_processors_ara.cpp \
+ /usr/share/juce/modules/juce_core/system/juce_CompilerWarnings.h \
+ /usr/share/juce/modules/juce_core/system/juce_TargetPlatform.h \
+ /usr/share/juce/modules/juce_audio_processors/utilities/ARA/juce_ARADebug.h \
+ /usr/share/juce/modules/juce_core/system/juce_PlatformDefs.h
---
+++ Builds/Linux/build/intermediate/Release/include_juce_audio_processors_lv2_libs_12bdca08.d
@@ -0,0 +1,5 @@
+build/intermediate/Release/include_juce_audio_processors_lv2_libs_12bdca08.o: \
+ ../../JuceLibraryCode/include_juce_audio_processors_lv2_libs.cpp \
+ ../../JuceLibraryCode/AppConfig.h \
+ ../../JuceLibraryCode/JucePluginDefines.h \
+ /usr/share/juce/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp
